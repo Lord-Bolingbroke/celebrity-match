f877832707f59355aa4886a8b57cd2f5a7391c0b
Jared Reando and Noah Schooler <noah@>
2018-10-18 11:49:06 -0700

    Add variables and text outputs to JS to fill survey results form.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("#survey").submit(function(event) {
+
+    var nameFirst = $("#nameFirst").val();
+    var nameLast = $("#nameLast").val();
+    var gender = $("input:radio[name=gender]:checked").val();
+    var age = $("#age").val();
+    var job = $("#job").val();
+
+    $(".nameFirst").text(nameFirst);
+    $(".nameLast").text(nameLast);
+    $(".gender").text(gender);
+    $(".age").text(age);
+    $(".job").text(job);
+
+    // debugger;
+    // $(".result").slideToggle().removeClass("hidden");
+    event.preventDefault();
+  });
+});
